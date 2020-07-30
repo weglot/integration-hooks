4a6d92ad88796c592fc7f3f5571840d9d29d5ee3
Nicolas de Chevigné <nico2che@gmail.com>
2020-07-30 10:20:16 +0200

    Export config

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apiKey: "", // weglot api_key
+
+  wflogin: "", // wflogin cookie content
+  wfsession: "", // wfsession cookie content
+
+  originalHost: "https://www.weglot-translate-wbflow.com/",
+  translatedHost: "https://LANG_CODE.weglot-translate-wbflow.com/",
+  languages: ["fr"],
+
+  projectName: "", // url slug
+};
--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const axios = require("axios");
-
-// ---
-
-const config = {
-  apiKey: "", // weglot api_key
-  wflogin: "", // wflogin cookie content
-  wfsession: "", // wfsession cookie content
-  originalHost: "https://www.weglot-translate-wbflow.com/",
-  translatedHost: "https://LANG_CODE.weglot-translate-wbflow.com/",
-  languages: ["fr"],
-  projectName: "", // url slug
-};
-
-// ---
+const config = require("./config");
 
 if (Object.values(config).some((s) => !s)) {
   console.log("Some config is missing");
