94fc10eab978675b052eb60b6c6c1f211e72b76f
Nicolas de Chevigné <nico2che@gmail.com>
2021-05-18 20:07:18 +0200

    Logs

--- webflow/config.js
+++ webflow/config.js
@@ -3,7 +3,6 @@ const global = require("../config");
 module.exports = {
   ...global,
 
-  wflogin: "1621349944137", // wflogin cookie content
-  wfsession:
-    "RyPQfwPy9uqIOkVWwKQiUQ.24mJ7YWlM97y35hRG4hcUBp2036bzY5ce9hX7Vgio0OQh0CCFug7ucCbjXPwN7hYd4CaNSo85rwn6KuWh5KdBgnvMoAL66tW-CiYLNEFRcru1Mckw8Orh1ksZgxijebqZTh6qDXMYdBk6CC75VxzZoRXRpYSCXKrZWCUlOpO9I_7Mc6eZX4CMca9w85eK5S4.1621943399560.86400000.6ZAKtHTFt6vxlCI3TEVcqxh-bGHTu08EQY-Y8Zhuyp8", // wfsession cookie content
+  wflogin: "", // wflogin cookie content
+  wfsession: "", // wfsession cookie content
 };
