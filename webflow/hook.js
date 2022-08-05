f162440a7685ee634cc18d12bc69db79a80945b6
roptch <ropichot@gmail.com>
2022-08-05 17:08:38 +0200

    pt-br language code exception

--- webflow/hook.js
+++ webflow/hook.js
@@ -60,11 +60,14 @@ function snippet(page) {
 
   const tags = languages
     .filter(({ connect_host_destination }) => connect_host_destination)
-    .map(({ language_to, custom_code, connect_host_destination }) =>
-      hreflang(
-        `https://${connect_host_destination.host}${getSlug(page, language_to)}`,
-        custom_code || language_to
-      )
+    .map(({ language_to, custom_code, connect_host_destination }) => {
+        const hreflang = custom_code || (language_to === "br" ? "pt-br" : "br");
+
+        return hreflang(
+          `https://${connect_host_destination.host}${getSlug(page, language_to)}`,
+          hreflang
+        );
+      }
     )
     .join("");
 
