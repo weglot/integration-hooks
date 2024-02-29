9f1100fc6ef243878a35baf6237c7d1205c4d583
Nicolas de Chevigné <nico2che@gmail.com>
2024-02-29 13:47:11 +0100

    Update config file

--- webflow/config.js
+++ webflow/config.js
@@ -2,8 +2,6 @@ module.exports = {
   apiKey: "", // weglot api_key
 
   wflogin: "", // wflogin cookie content
-  wfsession: "", // wfsession cookie content
-  projectName: "", // url slug
-
-  overwrite: false, // append or overwrite custom <head> content, default to append
+  wfdesignersession: "", // wfdesignersession cookie content
+  projectId: "", // url subdomain
 };
